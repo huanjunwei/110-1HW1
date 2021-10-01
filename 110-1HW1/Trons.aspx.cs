9ad9e0337a0cadc86a1a2211e82548f5dc7513be
俊瑋 黃 <108111105@mail.aeust.edu.tw>
2021-10-01 18:27:44 +0800

    3.982

---
+++ 110-1HW1/Trons.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW1
+{
+    public partial class Trons : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double c, d_Value;
+            d_Value = 3.982;
+            c = d_Value * d_Value;
+            Response.Write("d_Value =" + d_Value + "*" + d_Value + "=" +c);
+        }
+    }
+}
